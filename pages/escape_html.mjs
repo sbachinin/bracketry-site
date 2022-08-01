cb975474df992f5d3071f14ff42ff4c870df342a
Sergey Bachinin <sbachinin@yandex.ru>
2022-08-01 16:16:01 +0300

    paste all stuff from library repo

---
+++ pages/escape_html.mjs
@@ -0,0 +1,8 @@
+export const escape_Html = unsafe => {
+    return String(unsafe)
+        ?.replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
