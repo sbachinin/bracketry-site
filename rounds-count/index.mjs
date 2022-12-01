47ff1621bdf7b342d2637d3bab40b604a52d999f
Sergey Bachinin <sbachinin@yandex.ru>
2022-12-01 20:44:02 +0530

    add rounds-count page

---
+++ rounds-count/index.mjs
@@ -0,0 +1,43 @@
+import { createPlayoffs } from '../easy-playoffs.min.js'
+import data from '../test_data/finished-singles-2021.js'
+import { adjust_options_on_resize } from '../options/adjust_options_on_resize.mjs'
+
+const playoffs = createPlayoffs(
+    data,
+    document.querySelector('.wr1'),
+    {
+        getNationalityHTML: p => `<div style="
+        width: 28px;
+        text-align: center;
+        font-size: 14px;
+        background: #f0f0f0;">${p.nationality}</div>`,
+        getEntryStatusHTML: es => `<div style="
+        width: 24px;
+        font-size: 14px;
+        text-align: center;">${es || ''}</div>`,
+        visibleRoundsCount: 1
+    }
+)
+
+adjust_options_on_resize(playoffs)
+
+
+
+const playoffs2 = createPlayoffs(
+    data,
+    document.querySelector('.wr2'),
+    {
+        getNationalityHTML: p => `<div style="
+        width: 28px;
+        text-align: center;
+        font-size: 14px;
+        background: #f0f0f0;">${p.nationality}</div>`,
+        getEntryStatusHTML: es => `<div style="
+        width: 24px;
+        font-size: 14px;
+        text-align: center;">${es || ''}</div>`,
+        displayWholeRounds: true
+    }
+)
+
+adjust_options_on_resize(playoffs2)
