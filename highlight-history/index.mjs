f14d8988c8a684fd443992ee5ef3074529ee5543
Sergey Bachinin <sbachinin@yandex.ru>
2022-12-01 18:34:26 +0530

    add highlight-history page

--- highlight-history/index.mjs
+++ highlight-history/index.mjs
@@ -1,16 +1,15 @@
 import { createPlayoffs } from '../easy-playoffs.min.js'
 import data from '../test_data/unfinished-tennis-doubles.js'
-import { add_options_manager } from '../options-manager/options-manager.mjs'
 import { adjust_options_on_resize } from '../options/adjust_options_on_resize.mjs'
 
 const playoffs = createPlayoffs(
     data,
-    document.querySelector('.common-playoffs-wrapper'),
+    document.querySelector('.wr1'),
+    {
+        highlightedConnectionLinesColor: 'pink',
+        highlightedPlayerTitleColor: '#8f04ff',
+        connectionLinesWidth: 5
+    }
 )
 
-const options_manager = add_options_manager(
-    document.querySelector('.options-manager-opener'),
-    playoffs
-)
-
-adjust_options_on_resize(playoffs, options_manager)
+adjust_options_on_resize(playoffs)
