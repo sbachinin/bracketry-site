0fa60d0206372b5ef3227b9b2778015f40457655
Sergey Bachinin <sbachinin@yandex.ru>
2023-03-24 20:18:34 +0530

    add snooker 3 example

--- examples/snooker3/index.mjs
+++ examples/snooker3/index.mjs
@@ -2,7 +2,18 @@ import { createBracket } from '../../bracketry.min.js'
 
 
 const data = {
-    rounds: [{}, {}, {}],
+    rounds: [
+        {
+            title: 'QF',
+            subtitle: 'Losers receive £15,000'
+        }, {
+            title: 'SF',
+            subtitle: 'Losers receive £30,000'
+        }, {
+            title: 'Final',
+            subtitle: 'Winner: £125,000; Runner-up: £50,000'
+        }
+    ],
     matches: [
         {
             roundIndex: 1,
@@ -10,14 +21,15 @@ const data = {
             sides: [
                 {
                     contestantId: '1038',
-                    scores: [ { mainScore: 4 } ]
+                    scores: [{ mainScore: 4 }]
                 },
                 {
                     contestantId: '158',
-                    scores: [ { mainScore: 6, isWinner: true } ],
+                    scores: [{ mainScore: 6, isWinner: true }],
                     isWinner: true
                 }
-            ]
+            ],
+            detailsUrl: 'https://livescores.worldsnookerdata.com/Matches/Result/14611/893944'
         },
         {
             roundIndex: 1,
@@ -25,14 +37,15 @@ const data = {
             sides: [
                 {
                     contestantId: '39',
-                    scores: [ { mainScore: 3 } ]
+                    scores: [{ mainScore: 3 }]
                 },
                 {
                     contestantId: '97',
-                    scores: [ { mainScore: 6, isWinner: true } ],
+                    scores: [{ mainScore: 6, isWinner: true }],
                     isWinner: true
                 }
-            ]
+            ],
+            detailsUrl: 'https://livescores.worldsnookerdata.com/Matches/Result/14611/893945'
         },
         {
             roundIndex: 2,
@@ -40,14 +53,15 @@ const data = {
             sides: [
                 {
                     contestantId: '158',
-                    scores: [ { mainScore: 4 } ]
+                    scores: [{ mainScore: 4 }]
                 },
                 {
                     contestantId: '97',
-                    scores: [ { mainScore: 10, isWinner: true } ],
+                    scores: [{ mainScore: 10, isWinner: true }],
                     isWinner: true
                 }
-            ]
+            ],
+            detailsUrl: 'https://livescores.worldsnookerdata.com/Matches/Result/14611/893946'
         },
     ],
     contestants: {
@@ -95,6 +109,15 @@ const wrapper_el = document.querySelector('.bracket-wrapper')
 
 
 const options = {
+    getRoundTitleElement: (roundData, roundIndex) => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+            <div class="rtitle">${roundData.title}</div>
+            <div class="rsubtitle">${roundData.subtitle}</div>
+        `
+        return div
+    },
+
     getNationalityHTML: p => {
         return `<img src="https://www.snooker.org/res/scorekeeper/gfx/flags/icondrawer/16/${p.nationality}.png">`
     },
@@ -106,20 +129,31 @@ const options = {
             </a>
         `
     },
-    
+
+    getMatchBottomHTML: match => {
+        const id1 = match.sides[0].contestantId
+        const id2 = match.sides[1].contestantId
+        return `
+            <div class="mbottom">
+                <a href="https://www.snooker.org/res/index.asp?player1=${id1}&player2=${id2}&season=-1">h2h</a>
+                <a href="${match.detailsUrl}">match details</a>
+            </div>
+        `
+    },
+
     disableHighlight: true,
     useClassicalLayout: true,
     displayWholeRounds: true,
     navButtonsPosition: 'overTitles',
 
     rootBorderColor: 'transparent',
-    
+
     matchFontSize: 12,
     matchHorMargin: 6,
     distanceBetweenScorePairs: 6,
     mainVerticalPadding: 0,
     roundTitlesFontSize: 18,
-    matchMinVerticalGap: 12,
+    matchMinVerticalGap: 24,
     matchAxisMargin: 2,
 }
 
