32ac553bcbf344aafcedc06bfa16f864374924a3
Sergei Bachinin <ssbachinin@gmail.com>
2024-08-20 15:28:53 +0700

    add atptour-fullscreen example

---
+++ examples/atptour-fullscreen/index.mjs
@@ -0,0 +1,26 @@
+import { createBracket } from '../../bracketry.min.js'
+import data from '../../test_data/finished-singles-2021.js'
+
+const bracket = createBracket(
+    data,    
+    document.querySelector('.common-bracket-wrapper'),
+    {
+        getNationalityHTML: p => {
+            const url = `https://flagcdn.com/16x12/${p.nationality?.toLowerCase()}.png`
+            return `<img style="width: 16px; margin-right: 5px;" src="${url}">`
+        },
+        // matchStatusBgColor: '#051224',
+        getEntryStatusHTML: es => {
+            return `<div style="
+                color: #969b9d;
+                width: 24px;
+                font-family: 'Roboto', sans-serif;
+                font-size: 13px;
+                text-align: center;">${es || ''}</div>`
+        },
+        matchFontSize: 13,
+        rootFontFamily: 'Roboto, sans-serif',
+        matchHorMargin: 6,
+        distanceBetweenScorePairs: 10
+    }
+)
