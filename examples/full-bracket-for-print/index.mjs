4980cfed94cd28ca7ebeac699262a15edba99495
Sergei Bachinin <ssbachinin@gmail.com>
2024-10-13 17:21:35 +0700

    add full-bracket-for-print example

---
+++ examples/full-bracket-for-print/index.mjs
@@ -0,0 +1,27 @@
+import { createBracket } from '../../bracketry.min.js'
+import data from '../../test_data/finished-singles-2021.js'
+
+createBracket(
+    data,    
+    document.querySelector('.common-bracket-wrapper'),
+    {
+        getNationalityHTML: p => {
+            const url = `https://flagcdn.com/16x12/${p.nationality?.toLowerCase()}.png`
+            return `<img style="width: 16px; margin-right: 5px;" src="${url}">`
+        },
+        getEntryStatusHTML: es => {
+            return `<div style="
+                color: #969b9d;
+                width: 24px;
+                font-family: 'Roboto', sans-serif;
+                font-size: 13px;
+                text-align: center;">${es || ''}</div>`
+        },
+        matchFontSize: 13,
+        rootFontFamily: 'Roboto, sans-serif',
+        matchHorMargin: 6,
+        distanceBetweenScorePairs: 10,
+        width: 'max-content',
+        useClassicalLayout: true,
+    }
+)
